ca035af6d26ec820e0aafb2c6120b0c2d370d303
heroisuseless <35455575+HeroIsUseless@users.noreply.github.com>
2024-11-25 09:41:06 +0800

    fidx

--- src/lib/final/index.ts
+++ src/lib/final/index.ts
@@ -1,8 +1,10 @@
+import { MouseManager } from "./core/mouse";
+// 其实问题还是挺大的
 export class Editor {
     // api
     public canvasApi: any;
     // core
-    private mouseController: any;
+    private mouseManager: MouseManager;
     constructor() {}
 
 }
